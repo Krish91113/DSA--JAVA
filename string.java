498f94842640930c443eb339fe6a1ddd83ea9c79
Krish1212 <krishbaranwal56@gmail.com>
2025-07-01 13:38:18 +0530

    Initial commit - DSA JAVA solutions

--- string.java
+++ string.java
@@ -101,25 +101,55 @@
 //         }
 // }
 // Convert letters to uppercase
-public class string {
-        public static String toUpperCase(String str){
-                StringBuilder sb = new StringBuilder("");
-                char ch = Character.toUpperCase(str.charAt(0));
-                sb.append(ch);
-                for(int i=1;i<str.length();i++){
-                        if(str.charAt(i) == ' ' && i<str.length()-1){
-                                sb.append(str.charAt(i));
-                                i++;
-                                sb.append(Character.toUpperCase(str.charAt(i)));
-                        }else{
-                                sb.append(str.charAt(i));
-                        }
-                }
-                return sb.toString();
-        }
+// public class string {
+//         public static String toUpperCase(String str){
+//                 StringBuilder sb = new StringBuilder("");
+//                 char ch = Character.toUpperCase(str.charAt(0));
+//                 sb.append(ch);
+//                 for(int i=1;i<str.length();i++){
+//                         if(str.charAt(i) == ' ' && i<str.length()-1){
+//                                 sb.append(str.charAt(i));
+//                                 i++;
+//                                 sb.append(Character.toUpperCase(str.charAt(i)));
+//                         }else{
+//                                 sb.append(str.charAt(i));
+//                         }
+//                 }
+//                 return sb.toString();
+//         }
 
-        public static void main(String[] args) {
-                String str = "hi , i am krish";
-                System.out.println(toUpperCase(str));
-        }
+//         public static void main(String[] args) {
+//                 String str = "hi , i am krish";
+//                 System.out.println(toUpperCase(str));
+//         }
+// }
+
+// leetcode 709 : TO lowerCase
+// public class Solution {
+//         public static String toLowerCase(String str){
+//                 StringBuilder sb = new StringBuilder("");
+//                 for(int i=0;i<str.length();i++){
+//                         if(str.charAt(i)>='A' && str.charAt(i)<='Z'){
+//                                 sb.append(Character.toLowerCase(str.charAt(i)));
+//                         }else{
+//                                 sb.append(str.charAt(i));
+//                         }
+//                 }
+//                 return sb.toString();
+//         }
+
+//         public static void main(String[] args) {
+//                 String str = "Hello";
+//                 System.out.println(toLowerCase(str));
+//         }
+// }
+public class string {
+    public static String toLowerCase(String s) {
+        String str = s.toLowerCase();
+        return str;
+    }
+    public static void main(String[] args) {
+        String str = "KRISH";
+        System.out.println(toLowerCase(str));
+    }
 }
