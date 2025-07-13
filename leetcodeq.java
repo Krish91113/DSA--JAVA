fafd722b0352c938999354ab9a5249257f3f93a5
Krish Baranwal <menikabaranwal121@gmail.com>
2025-07-13 17:43:46 +0530

    solved max product subarray

--- leetcodeq.java
+++ leetcodeq.java
@@ -496,3 +496,42 @@
 //         return false;
 //     }
 // }
+// Max Subarray sum
+// public class array {
+//     public static int maxSubArray(int nums[]){
+//         int ans = Integer.MIN_VALUE;
+//         int sum = 0;
+//         for(int i=0;i<nums.length;i++){
+//             sum+=nums[i];
+//             ans = Math.max(ans, sum);
+//             if(sum<0){
+//                 sum = 0;
+//             }
+//         }
+//         return ans;
+//     }
+//     public static void main(String[] args) {
+//         int nums[] = {2,3,-7,11,-1,3};
+//         System.out.println(maxSubArray(nums));
+//     }
+// }
+
+// Max Product array
+public class leetcodeq {
+    public static int maxProductSubarray(int nums[]){
+        int product = 1;
+        int answer = Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++){
+            product = product * nums[i];
+            answer = Math.max(answer, product);
+            if(product < 0){
+                product = 1;
+            }
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        int nums[] ={-2,0,-1};
+        System.out.println(maxProductSubarray(nums));
+    }
+}
