fe93628c49e4d517331a4135c082a0c0d1baaaab
Krish Baranwal <menikabaranwal121@gmail.com>
2025-07-15 19:49:04 +0530

    solved tranpose matrix

--- twodimensionalarray.java
+++ twodimensionalarray.java
@@ -192,14 +192,22 @@
 // Transpose of matrix
 public class twodimensionalarray {
     public static void transpose(int arr[][]){
-        for(int col=0;col<arr[0].length;col++){
-            for(int row=0;row<arr.length;row++){
-                System.out.print(arr[row][col] +" ");
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr[0].length;j++){
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
             }
         }
     }
     public static void main(String[] args) {
         int arr[][] = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
         transpose(arr);
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[0].length;j++){
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
