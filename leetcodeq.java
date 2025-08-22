28df992687afbb17125651f71f8b0203674f69cc
Krish Baranwal <menikabaranwal121@gmail.com>
2025-08-22 18:37:15 +0530

    Solved one lc question

--- leetcodeq.java
+++ leetcodeq.java
@@ -1575,20 +1575,49 @@
 //     }
 // }
 
-// Toeplix matrix
-public class leetcodeq{
-    public static boolean isToeplitzMatrix(int[][] matrix) {
-        for(int i = 0; i < matrix.length - 1; i++) {
-            for(int j = 0; j < matrix[0].length - 1; j++) {
-                if(matrix[i][j] != matrix[i + 1][j + 1]) {
-                    return false; 
+// // Toeplix matrix
+// public class leetcodeq{
+//     public static boolean isToeplitzMatrix(int[][] matrix) {
+//         for(int i = 0; i < matrix.length - 1; i++) {
+//             for(int j = 0; j < matrix[0].length - 1; j++) {
+//                 if(matrix[i][j] != matrix[i + 1][j + 1]) {
+//                     return false; 
+//                 }
+//             }
+//         }
+//         return true;
+//     }
+//     public static void main(String[] args) {
+//         int[][] matrix = {{1,2,3,4},{5,1,2,3},{9,5,1,2}};
+//         System.out.println(isToeplitzMatrix(matrix));
+//     }
+// }
+
+// find min area to cover all ones
+public class leetcodeq {
+
+    public static int minimumArea(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int minrow = m;
+        int maxrow = -1;
+        int mincol = n;
+        int maxcol = -1;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j] == 1){
+                    minrow = Math.min (minrow,i);
+                    maxrow = Math.max(maxrow,i);
+                    mincol = Math.min (mincol,j);
+                    maxcol = Math.max(maxcol,j);
                 }
             }
         }
-        return true;
+        return (maxrow-minrow+1)* (maxcol-mincol+1);
     }
+
     public static void main(String[] args) {
-        int[][] matrix = {{1,2,3,4},{5,1,2,3},{9,5,1,2}};
-        System.out.println(isToeplitzMatrix(matrix));
+        int grid[][] = {{0,1,0},{1,0,1}};
+        System.out.println(minimumArea(grid));
     }
 }
