f1daf046f22035d146380c520c60b9e6467b4791
Krish Baranwal <menikabaranwal121@gmail.com>
2025-08-31 10:51:32 +0530

    K

--- leetcodecontest.java
+++ leetcodecontest.java
@@ -5,7 +5,7 @@ public class leetcodecontest {
         long ans = 0;
         for(int i=0;i<nums.length;i++)  {
             
-            for(int j=0;j<nums.length;j++){
+            for(int j=i+1;j<nums.length;j++){
                 if((nums[i] & nums[j]) == 0){
                     product = nums[i] * nums[j];
                     ans = Math.max(ans, product);
@@ -15,7 +15,7 @@ public class leetcodecontest {
         return ans;
     }
     public static void main(String[] args) {
-        int nums [] = {5,6,4};
+        int nums [] = {1,2,4,8,16,32,64,128,256,512,1024,2048,4096,8192,16384,32768,65536,131072,262144,524288,1048576};
         System.out.println(maxProduct(nums));
     }
 }
