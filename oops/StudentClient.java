294c4efb904b3a487571e6070a7928a3f7dab503
Krish Baranwal <menikabaranwal121@gmail.com>
2025-09-01 20:23:11 +0530

    S

---
+++ oops/StudentClient.java
@@ -0,0 +1,13 @@
+package oops;
+
+import java.util.Scanner;
+
+public class StudentClient {
+    public static void main(String[] args) {
+        student s = new student();
+        s.age=20;
+        s.name="Ankit";
+        s.Intro_yourSelf();
+
+    }
+}
---
+++ oops/student.java
@@ -0,0 +1,10 @@
+package oops;
+
+public class student {
+
+    String name ;
+    int age ;
+    public void Intro_yourSelf(){
+        System.out.println("My name is " + name + " and I am " + age + " years old.");
+    }    
+}
