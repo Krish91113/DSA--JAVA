1fe41bb4664ed7c3fc41545ce5c9b837ab0e6e06
Krish Baranwal <menikabaranwal121@gmail.com>
2025-09-09 07:18:21 +0530

    learning

--- oops/Car.java
+++ oops/Car.java
@@ -1,19 +1,34 @@
 package oops;
 
 public class Car {
-    String color;
-    String model;
-    int speed;
-    int price;
+    private String color;
+    private String model;
+    private int speed;
+    private int price;
 
-    public Car(String color, String model, int speed, int price) {
-        this.color = color;
-        this.model = model;
-        this.speed = speed;
+    // public Car(String color, String model, int speed, int price) {
+    //     this.color = color;
+    //     this.model = model;
+    //     this.speed = speed;
+    //     this.price = price;
+    // }
+    // public void DisplayCar(){
+    //     System.out.println("c " + color + " m " + model + " p " + price + " s " + speed );
+    // }
+    // public void Setcolor(String color){
+    //     this.color = color;
+    //     System.out.println(color);
+    // }
+    // public String Setcolor(){
+    //     return this.color;
+    // }
+
+    public void price (int price)throws Exception{
+        if(price < 0){
+            throw new Exception("Price kabhi negative nai hota mitra");
+        }
         this.price = price;
-    }
-    public void DisplayCar(){
-        System.out.println("c " + color + " m " + model + " p " + price + " s " + speed );
+        System.out.println(price);
     }
 }
 
--- oops/Car_Client.java
+++ oops/Car_Client.java
@@ -2,13 +2,13 @@ package oops;
 
 public class Car_Client {
     
-    public static void main(String[] args) {
-        Car c = new Car("Black","Green",1299,109);
-        Car c1 = new Car("White","Green",123299,1019);
-        c.DisplayCar();
-        c.price = -13951;
-        c.DisplayCar();
-        c1.DisplayCar();
+    public static void main(String[] args) throws Exception{
+        Car c = new Car();
+        Car c1 = new Car();
+       
+        c.price(1000);
+        c1.price(-100);
+        
 
     }
 }
