020a03a4df2c389a7762b473eaf58149d9755718
Krish Baranwal <menikabaranwal121@gmail.com>
2025-09-19 20:57:56 +0530

    Solved

--- leetcodequestions/threesum.java
+++ leetcodequestions/threesum.java
@@ -4,27 +4,28 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.*;
 public class threesum {
-    public static List<List<Integer>> threeSum(int[] nums){
-        if(nums==null || nums.length < 3) return new ArrayList<>();
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);
-        Set<List<Integer>> result = new HashSet<>();
-        for(int i=0;i<nums.length-2;i++){
-            int left = i+1;
-            int right = nums.length-1;
-            while(left < right){
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // Skip duplicates
+            int left = i + 1, right = nums.length - 1;
+            while (left < right) {
                 int sum = nums[i] + nums[left] + nums[right];
-                if(sum == 0){
-                    result.add(Arrays.asList(nums[i],nums[left],nums[right]));
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    while (left < right && nums[left] == nums[left + 1]) left++; // Skip duplicates
+                    while (left < right && nums[right] == nums[right - 1]) right--; // Skip duplicates
                     left++;
                     right--;
-                } else if(sum < 0){
+                } else if (sum < 0) {
                     left++;
                 } else {
                     right--;
                 }
             }
         }
-        return new ArrayList<>(result);
+        return result;
     }
     public static void main(String[] args) {
         int [] nums = {-1,0,1,2,-1,-4};
