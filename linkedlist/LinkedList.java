610c9e9d7190d0882674132da1fefc06854affe4
Krish Baranwal <menikabaranwal121@gmail.com>
2025-09-21 18:31:23 +0530

    solved

---
+++ linkedlist/LinkedList.java
@@ -0,0 +1,28 @@
+package linkedlist;
+
+public class LinkedList {
+    public static class Node{
+        int data;
+        Node next;
+
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+
+    // add first
+    public void addFirst(int data){
+        // create new node 
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+    }
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        ll.addFirst(1);
+        System.out.println(ll.head.data);
+    }
+}
