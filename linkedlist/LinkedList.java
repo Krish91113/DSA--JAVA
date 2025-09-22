c26b4ab77018212d0a63739ec69bba4b9930d210
Krish Baranwal <menikabaranwal121@gmail.com>
2025-09-22 07:15:27 +0530

    learning

--- linkedlist/LinkedList.java
+++ linkedlist/LinkedList.java
@@ -17,12 +17,20 @@ public class LinkedList {
     public void addFirst(int data){
         // create new node 
         Node newNode = new Node(data);
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+        // newNode next = head
         newNode.next = head;
+        // head = newNode
         head = newNode;
     }
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
         ll.addFirst(1);
+        ll.addFirst(2);
+        ll.addFirst(3);
         System.out.println(ll.head.data);
     }
 }
