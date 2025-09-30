b08af87079240bcc241a2332187feb076d37a2ae
Krish Baranwal <menikabaranwal121@gmail.com>
2025-09-30 07:12:00 +0530

    learned

--- linkedlist/LinkedList.java
+++ linkedlist/LinkedList.java
@@ -54,11 +54,27 @@ public class LinkedList {
         head = head.next;
         return val;
     }
+
+    public int itrSearch(int key){
+        Node temp = head;
+        int i=0;
+        while(temp != null){
+            if(temp.data == key){
+                return i;
+            }
+            temp = temp.next;
+            i++;
+        }
+        return -1;  
+    }
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
-        ll.addFirst(2);
-        ll.addLast(1);
-        ll.addFirst(3);
+        ll.addFirst(1);
+        ll.addLast(2);
+        ll.addLast(3);
+        ll.addLast(4);
         ll.print();
+        System.out.println(ll.itrSearch(3));
+        System.out.println(ll.itrSearch(10));
     }
 }
