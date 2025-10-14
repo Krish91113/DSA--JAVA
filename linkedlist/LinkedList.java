28e74539be377c7dd1c6457294d123046ec23d20
Krish Baranwal <menikabaranwal121@gmail.com>
2025-10-14 05:44:54 +0530

    larning

--- linkedlist/LinkedList.java
+++ linkedlist/LinkedList.java
@@ -131,7 +131,7 @@ public class LinkedList {
        head = new Node(1);
        head.next = new Node(2);
        head.next.next= new Node(3);
-       head.next.next.next= head;
+    //    head.next.next.next= head;
        //1->2->3->1
        System.out.println(isCycle());
     }
