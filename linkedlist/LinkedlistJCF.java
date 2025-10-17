e3ffe4e8285e5d0bf17bbce97af0be96a3ceeec3
Krish Baranwal <menikabaranwal121@gmail.com>
2025-10-17 07:01:45 +0530

    Learning LL

---
+++ linkedlist/LinkedlistJCF.java
@@ -0,0 +1,11 @@
+import java.util.LinkedList;
+
+public class LinkedlistJCF {
+    public static void main(String[] args) {
+        LinkedList<Integer> ll = new LinkedList<>();
+        ll.addFirst(10);
+        ll.addLast(20);
+        ll.addLast(30);
+        System.out.println(ll);
+    }
+}
