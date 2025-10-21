fd6ecfd1ac9006dba65a8392f12603a996bd8197
Krish Baranwal <menikabaranwal121@gmail.com>
2025-10-21 17:06:06 +0530

    L

--- leetcodedaily.java
+++ leetcodedaily.java
@@ -260,21 +260,53 @@
 
 // final value of variables after performing operations
 
-public class leetcodedaily {
+// public class leetcodedaily {
 
-    public static int finalValueAfterOperations(String[] operations) {
-        int x = 0;
-        for (String op : operations) {
-            if (op.equals("++X") || op.equals("X++")) {
-                x++;
-            } else if (op.equals("--X") || op.equals("X--")) {
-                x--;
-            }
+//     public static int finalValueAfterOperations(String[] operations) {
+//         int x = 0;
+//         for (String op : operations) {
+//             if (op.equals("++X") || op.equals("X++")) {
+//                 x++;
+//             } else if (op.equals("--X") || op.equals("X--")) {
+//                 x--;
+//             }
+//         }
+//         return x;
+//     }
+//     public static void main(String[] args) {
+//         String operations[] = {"--X","X++","X++"};
+//         System.out.println(finalValueAfterOperations(operations));
+//     }
+// }
+
+public class leetcodedaily{
+
+    public static int maxFrequency(int[] nums, int k, int ops) {
+        int mx = Arrays.stream(nums).max().getAsInt();
+        int n = mx + k + 2;
+        int[] f = new int[n];
+        for (int x : nums) f[x]++;
+
+        int[] pre = new int[n];
+        pre[0] = f[0];
+        for (int i = 1; i < n; i++) pre[i] = pre[i - 1] + f[i];
+
+        int ans = 0;
+        for (int t = 0; t < n; t++) {
+            if (f[t] == 0 && ops == 0) continue;
+            int l = Math.max(0, t - k), r = Math.min(n - 1, t + k);
+            int tot = pre[r] - (l > 0 ? pre[l - 1] : 0);
+            int adj = tot - f[t];
+            int val = f[t] + Math.min(ops, adj);
+            ans = Math.max(ans, val);
         }
-        return x;
+        return ans;
     }
+
     public static void main(String[] args) {
-        String operations[] = {"--X","X++","X++"};
-        System.out.println(finalValueAfterOperations(operations));
+        int nums[] = {1,2,4};
+        int k = 5;
+        int ops = 1;
+        System.out.println(maxFrequency(nums, k, ops));
     }
 }
