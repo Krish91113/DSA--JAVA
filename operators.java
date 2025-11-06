ea23267a70204a125ffd5849fece53d5108343ad
Krish Baranwal <menikabaranwal121@gmail.com>
2025-11-06 19:23:14 +0530

    solved valid mountain

--- operators.java
+++ operators.java
@@ -10,21 +10,42 @@ public class operators {
 //     System.out.println("Divide = " + (a/b));
 //     System.out.println("Modulus = " + (a %b));
 // }
-public static int twoSums(int arr[] ,int target){
-    int sums = 0;
-    int index = 0;
-    for(int i =0 ; i<arr.length;i++){
-        index = i;
-        sums = sums + arr[i];
-        if(sums == target){
-          System.out.println(i);
+// public static int twoSums(int arr[] ,int target){
+//     int sums = 0;
+//     int index = 0;
+//     for(int i =0 ; i<arr.length;i++){
+//         index = i;
+//         sums = sums + arr[i];
+//         if(sums == target){
+//           System.out.println(i);
+//         }
+//     }
+//     return sums;
+// }
+// public static void main(String[] args) {
+//     int arr[] = {2,7,11,15};
+//     int target = 9;
+//     twoSums(arr, target);
+// }
+// 
+
+
+
+    public static boolean validMountainArray(int[] arr) {
+        int i=0,n=arr.length;
+        while(i+1 < n && arr[i]<arr[i+1]){
+            i++;
         }
+        if(i==0||i==n-1){
+            return false;
+        }
+        while(i+1<n && arr[i]>arr[i+1]){
+            i++;
+        }
+        return i==n-1;
     }
-    return sums;
-}
-public static void main(String[] args) {
-    int arr[] = {2,7,11,15};
-    int target = 9;
-    twoSums(arr, target);
-}
+    public static void main(String[] args) {
+        int arr[] = {0,3,2,1};
+        System.out.println(validMountainArray(arr));
+    }
 }
