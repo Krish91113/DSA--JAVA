3e8b571e0ea64d30bcd33b5e8eba6b8f4ed61204
Krish Baranwal <menikabaranwal121@gmail.com>
2025-11-12 21:52:06 +0530

    l

--- operators.java
+++ operators.java
@@ -137,22 +137,66 @@ public class operators{
     // }
 
     
-    public static int findMaxForm(String[] S, int M, int N) {
-        int[][] dp = new int[M+1][N+1];
-        for (String str : S) {
-            int zeros = 0, ones = 0;
-            for (char c : str.toCharArray())
-                if (c == '0') zeros++;
-                else ones++;
-            for (int i = M; i >= zeros; i--)
-                for (int j = N; j >= ones; j--)
-                    dp[i][j] = Math.max(dp[i][j], dp[i-zeros][j-ones] + 1);
+    // public static int findMaxForm(String[] S, int M, int N) {
+    //     int[][] dp = new int[M+1][N+1];
+    //     for (String str : S) {
+    //         int zeros = 0, ones = 0;
+    //         for (char c : str.toCharArray())
+    //             if (c == '0') zeros++;
+    //             else ones++;
+    //         for (int i = M; i >= zeros; i--)
+    //             for (int j = N; j >= ones; j--)
+    //                 dp[i][j] = Math.max(dp[i][j], dp[i-zeros][j-ones] + 1);
+    //     }
+    //     return dp[M][N];
+    // }
+    // public static void main(String[] args) {
+    //    String strs []= {"10","0001","111001","1","0"};
+    //    int M = 5, N= 3;
+    //    System.out.println(findMaxForm(strs,M,N));
+    // }
+
+    public static int minOperations(int[] nums) {
+        int n = nums.length;
+        int num1 = 0;
+        int g = 0;
+        for (int x : nums) {
+            if (x == 1) {
+                num1++;
+            }
+            g = gcd(g, x);
         }
-        return dp[M][N];
+        if (num1 > 0) {
+            return n - num1;
+        }
+        if (g > 1) {
+            return -1;
+        }
+
+        int minLen = n;
+        for (int i = 0; i < n; i++) {
+            int currentGcd = 0;
+            for (int j = i; j < n; j++) {
+                currentGcd = gcd(currentGcd, nums[j]);
+                if (currentGcd == 1) {
+                    minLen = Math.min(minLen, j - i + 1);
+                    break;
+                }
+            }
+        }
+        return minLen + n - 2;
+    }
+
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
     }
     public static void main(String[] args) {
-       String strs []= {"10","0001","111001","1","0"};
-       int M = 5, N= 3;
-       System.out.println(findMaxForm(strs,M,N));
+        int nums[]={2,6,3,4};
+        System.out.println(minOperations(nums));
     }
 }
