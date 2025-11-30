958fbec90fc24a2dabd96be415e1d35f331100e3
Krish Baranwal <menikabaranwal121@gmail.com>
2025-11-30 21:22:34 +0530

    solved

--- operators.java
+++ operators.java
@@ -267,16 +267,32 @@ public class operators{
 
 public class operators {
     
-    public static int minOperations(int[] nums, int k) {
-        int sum=0;
-        for(int i=0;i<nums.length;i++){
-            sum+=nums[i];
+    class Solution {
+    public int minSubarray(int[] nums, int p) {
+        long total = 0;
+        for (int num : nums) total += num;
+
+        long target = total % p;
+        if (target == 0) return 0;
+
+        Map<Integer, Integer> mMap = new HashMap<>();
+        mMap.put(0, -1);
+
+        long prefix = 0;
+        int res = nums.length;
+
+        for (int i = 0; i < nums.length; i++) {
+            prefix = (prefix + nums[i]) % p;
+            int need = (int)((prefix - target + p) % p);
+
+            if (mMap.containsKey(need)) {
+                res = Math.min(res, i - mMap.get(need));
+            }
+
+            mMap.put((int)prefix, i);
         }
-        return sum%k;
+
+        return res == nums.length ? -1 : res;
     }
-    public static void main(String[] args) {
-        int nums[] = {2,3,4,5};
-        int k = 3;
-        System.out.println(minOperations(nums,k));
-    }
+
 }   
