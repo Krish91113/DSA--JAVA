7ad44248b51f683ec198342031bbdc5a43bbbc0f
Krish Baranwal <menikabaranwal121@gmail.com>
2025-12-05 21:46:56 +0530

    k

--- leetcodedaily.java
+++ leetcodedaily.java
@@ -460,7 +460,24 @@
 // }
 
 public class leetcodedaily{
-    leftSum
+    class Solution {
+    public int countPartitions(int[] nums) {
+        int totalSum=0;
+        for(int i=0;i<nums.length;i++){
+            totalSum+=nums[i];
+        }
+        int leftSum=0,
+            count=0;
+        for(int i=0;i<nums.length-1;i++){
+            leftSum+=nums[i];
+            int rightSum=totalSum - leftSum;
+            if((leftSum % 2 ) == (rightSum % 2)){
+                count++;
+            }
+        }
+        return count;
+    }
+}
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
