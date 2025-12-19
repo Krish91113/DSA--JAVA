fccb6ea02784f9a14841533e418bb0f5f58de616
Krish Baranwal <menikabaranwal121@gmail.com>
2025-12-19 13:40:51 +0530

    solved two question

--- LinkedList/DisplayList.java
+++ LinkedList/DisplayList.java
@@ -141,3 +141,37 @@ class Solution {
         return d1.next;
     }
 }
+
+//partitiion a linked list around a value x such that all nodes less than x come before nodes greater than or equal to x.
+
+
+class Solution {
+    public static Node partition(Node head, int x) {
+        // Your code here
+        Node d1=new Node(-1);
+        Node d2=new Node(-1);
+        Node d3=new Node(-1);
+        Node t1=d1;
+        Node t2=d2;
+        Node t3=d3;
+        Node t=head;
+        while(t!=null){
+            if(t.data < x){
+                t1.next=t;
+                t1=t1.next;
+            }else if(t.data > x){
+                t3.next=t;
+                t3=t3.next;
+            }else{
+                t2.next=t;
+                t2=t2.next;
+            }
+            t=t.next;
+        }
+        t1.next=d2.next;
+        t2.next=d3.next;
+        t3.next=null;
+        
+        return d1.next;
+    }
+}
