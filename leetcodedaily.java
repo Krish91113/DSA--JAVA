f0aed81125826a31d4f8faf69ab9e12dc18a280f
Krish Baranwal <menikabaranwal121@gmail.com>
2025-12-21 13:58:09 +0530

    solved two question

--- leetcodedaily.java
+++ leetcodedaily.java
@@ -484,16 +484,3 @@
 //     }
 // }
 
-import Stacks.stacks;
-
-public class leetcodedaily{
-    
-   public static int countOdds(int low, int high) {
-        return (high + 1) / 2 - (low / 2);
-    }
-    public static void main(String[] args) {
-        int low = 3;
-        int high = 7;
-        System.out.println(countOdds(low, high));
-    }
-}
