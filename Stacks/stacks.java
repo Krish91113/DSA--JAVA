0c5792b63ffc496a4e8277b5b9f593a492036f33
Krish Baranwal <menikabaranwal121@gmail.com>
2026-01-02 14:58:30 +0530

    working

--- Stacks/stacks.java
+++ Stacks/stacks.java
@@ -1,49 +1,76 @@
-package Stacks;
+// package Stacks;
 
-import java.util.ArrayList;
-import java.util.Stack;
-
+// import java.util.ArrayList;
 // import java.util.Stack;
 
+// // import java.util.Stack;
+
+// // public class stacks {
+// //     public static void main(String[] args) {
+// //         Stack<Integer> st = new Stack<>();
+// //         st.push(10);
+// //         st.push(20);
+// //         st.push(30);
+// //         st.push(40);
+// //         st.push(50);
+// //         System.out.println(st.peek());
+// //         System.out.println((st));
+// //         System.out.println(st.isEmpty());
+// //         System.out.println(st.pop());
+// //         System.out.println(st);
+// //     }
+// // }
+
 // public class stacks {
-//     public static void main(String[] args) {
+//     public static void NGE(int arr[]){
+//          ArrayList<Integer> res = new ArrayList<>();
+//         int [] ans = new int[arr.length];
 //         Stack<Integer> st = new Stack<>();
-//         st.push(10);
-//         st.push(20);
-//         st.push(30);
-//         st.push(40);
-//         st.push(50);
-//         System.out.println(st.peek());
-//         System.out.println((st));
-//         System.out.println(st.isEmpty());
-//         System.out.println(st.pop());
-//         System.out.println(st);
+//         for(int i=0;i<arr.length;i++){
+//             while(!st.isEmpty() && arr[st.peek()] < arr[i]){
+//                 ans[st.pop()] = arr[i];
+//             }
+//             st.push(i);
+//         }
+//         while(!st.isEmpty()){
+//             ans[st.pop()] = -1;
+//         }
+//         for(int i=0;i<ans.length;i++){
+//             res.add(ans[i]);
+//         }   
+//         for(int i=0;i<res.size();i++){
+//             System.out.print(res.get(i)+" ");   
+//         }
+//     }
+//     public static void main(String[] args) {
+//         int []  arr = {55,11,9,7,13,3,66,60};
+//         NGE(arr);
 //     }
 // }
 
+ package Stacks;
+
+import java.util.Stack;
+
 public class stacks {
-    public static void NGE(int arr[]){
-         ArrayList<Integer> res = new ArrayList<>();
-        int [] ans = new int[arr.length];
-        Stack<Integer> st = new Stack<>();
-        for(int i=0;i<arr.length;i++){
-            while(!st.isEmpty() && arr[st.peek()] < arr[i]){
-                ans[st.pop()] = arr[i];
-            }
-            st.push(i);
-        }
-        while(!st.isEmpty()){
-            ans[st.pop()] = -1;
-        }
-        for(int i=0;i<ans.length;i++){
-            res.add(ans[i]);
-        }   
-        for(int i=0;i<res.size();i++){
-            System.out.print(res.get(i)+" ");   
-        }
-    }
+    
     public static void main(String[] args) {
-        int []  arr = {55,11,9,7,13,3,66,60};
-        NGE(arr);
+        Stack<Integer> st1 = new Stack<>();
+        int element=50;
+        st1.push(4);
+        st1.push(3);
+        st1.push(2);
+        st1.push(1);
+        st1.push(8);
+        pushAtBottom(st1, element);
+    }
+    private static void pushAtBottom(Stack<Integer> st, int element ){
+        if(st.size() == 0){
+            st.push(element);
+            return;
+        }
+        int top=st.pop();
+        pushAtBottom(st, element);
+        st.push(top);
     }
 }
