6669acdd9044c45b93246cbd02c1eca64cad0eb5
Krish Baranwal <menikabaranwal121@gmail.com>
2026-01-20 08:11:27 +0530

    learinng queue ds

--- Queue/findWinner.java
+++ Queue/findWinner.java
@@ -4,4 +4,20 @@ public class findWinner {
     public static void main(String[] args) {
         
     }
+    
+    public int findTheWinner(int n, int k) {
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=1;i<=n;i++){
+            q.add(i);
+        }
+        while(q.size()>1){
+            //k-1;
+            for(int i=1;i<=k-1;i++){
+                q.add(q.remove());
+            }
+            q.remove();
+        }
+        return q.peek();
+    }
+
 }
