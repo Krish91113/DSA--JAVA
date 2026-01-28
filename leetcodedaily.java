005969bab46058753ba8b3a0b1e1521de16de78b
Krish Baranwal <menikabaranwal121@gmail.com>
2026-01-28 22:14:55 +0530

    solve

--- leetcodedaily.java
+++ leetcodedaily.java
@@ -867,3 +867,19 @@
 //         return res;
 //     }
 // // }
+class Solution {
+    public int[] sumZero(int n) {
+        int[] res = new int[n];
+        int num = 1;
+        for (int i = 0; i < n / 2; i++) {
+            res[i] = num;
+            res[n - 1 - i] = -num;
+            num++;
+        }
+        // If n is odd, set the middle element to 0
+        if (n % 2 != 0) {
+            res[n / 2] = 0;
+        }
+        return res;
+    }
+}
