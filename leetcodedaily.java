8fca7d21dcbe19237bfb27aa280fc17293c4bfe7
Krish Baranwal <menikabaranwal121@gmail.com>
2026-01-31 09:29:27 +0530

    solve

--- leetcodedaily.java
+++ leetcodedaily.java
@@ -937,3 +937,23 @@
 //         return totalHours <= h;
 //     }
 // }
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        //['c', 'f' , 'j']
+        //  l    m     r
+        //mid=1
+        int l= 0,r=letters.length-1;
+        char ans  = letters[0];
+        while(l<=r){
+            int mid = l +(r-l) / 2;
+            if(letters[mid]>target){
+                ans = letters[mid];
+                r = mid -1 ;
+            }else{
+                l = mid + 1;
+               
+            }
+        }
+        return ans;
+    }
+}
