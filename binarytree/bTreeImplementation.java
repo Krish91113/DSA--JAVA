10e308ec1b6a028cb4d27b7586e2ce2fde37e3ab
Krish Baranwal <menikabaranwal121@gmail.com>
2026-02-06 21:31:31 +0530

    sinnglNumber done

---
+++ binarytree/bTreeImplementation.java
@@ -0,0 +1,40 @@
+
+class Node{
+        int val;
+        Node left;
+        Node right;
+        Node(int val){
+            this.val = val;
+        }
+    }
+public class bTreeImplementation {
+    
+    public static void main(String[] args) {
+        // Creating a binary tree with the following structure:
+        //         3
+        //        / \
+        //       4   2
+        //      / \  / \
+        //     -1  1 6  9
+        Node a = new Node(3);
+        Node b = new Node(4);
+        Node c = new Node(2);
+        Node d = new Node(-1);
+        Node e = new Node(1);
+        Node f = new Node(6);
+        Node g = new Node(9);
+        a.left = b;
+        a.right = c;
+        b.right=e;
+        b.left=d;
+        c.left=f;
+        c.right=g;
+        display(a);
+    }
+    public static void display(Node root){
+        if(root == null) return;
+        System.out.print(root.val + " ");
+        display(root.left);
+        display(root.right);
+    }
+}
