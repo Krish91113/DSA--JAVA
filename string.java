17b8346607ae8af3ac393cef32e595dd32a95e12
Krish Baranwal <menikabaranwal121@gmail.com>
2026-02-20 22:28:20 +0530

    done

--- string.java
+++ string.java
@@ -471,9 +471,9 @@
 //         return list;
 //     }
 // }
-class Solution {
-    public boolean hasAlternatingBits(int n) {
-        int x = (n ^ (n >> 1));
-        return (x & (x+1))==0;
-    }
-}
+// class Solution {
+//     public boolean hasAlternatingBits(int n) {
+//         int x = (n ^ (n >> 1));
+//         return (x & (x+1))==0;
+//     }
+// }
