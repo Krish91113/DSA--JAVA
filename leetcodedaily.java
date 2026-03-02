64e2aea15165954d22c0b3f755c08f31229fb8c7
Krish Baranwal <menikabaranwal121@gmail.com>
2026-03-02 08:53:44 +0530

    preorder traversal

--- leetcodedaily.java
+++ leetcodedaily.java
@@ -1043,3 +1043,33 @@ class Solution {
         return max;
     }
 }
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private static void dfs(TreeNode root, ArrayList<Integer> ans){
+        if(root == null ){
+            return ;
+        }
+        ans.add(root.val);
+        dfs(root.left,ans);
+        dfs(root.right,ans);
+    }
+    public List<Integer> preorderTraversal(TreeNode root) {
+        ArrayList<Integer> a = new ArrayList<>();
+        dfs(root,a);
+        return a;
+    }
+}
