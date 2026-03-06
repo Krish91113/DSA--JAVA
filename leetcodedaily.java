082f46127183f2c2524e4bc53a9fc09605153288
Krish Baranwal <menikabaranwal121@gmail.com>
2026-03-06 09:05:54 +0530

    potd done

--- leetcodedaily.java
+++ leetcodedaily.java
@@ -1091,3 +1091,14 @@ class Solution {
         return Math.min(p1,p2);
     }
 }
+class Solution {
+    public boolean checkOnesSegment(String s) {
+        
+        for(int i= 1;i<s.length();i++){
+            if(s.charAt(i-1) == '0' && s.charAt(i)== '1'){
+                return false;
+            }
+        }
+        return true;
+    }
+}
