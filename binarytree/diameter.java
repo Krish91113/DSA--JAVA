87c9cdc78b213b945526e43d913b4d98db902e1e
Krish Baranwal <menikabaranwal121@gmail.com>
2026-03-09 10:41:03 +0530

    one question able to solv

---
+++ binarytree/diameter.java
@@ -0,0 +1,38 @@
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    static int max;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        max = 0;
+        levels(root);
+        return max;
+    }
+
+    private static int levels(TreeNode root) {
+        if (root == null)
+            return 0;
+        int left = levels(root.left);
+        int right = levels(root.right);
+        max = Math.max(max, left + right);
+        return 1 + Math.max(right, left);
+    }
+}
