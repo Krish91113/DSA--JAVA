fdbfd102a12aa3c7d4a50ba6b65ba4ec3d3ce544
Krish Baranwal <menikabaranwal121@gmail.com>
2026-04-06 21:13:59 +0530

    todays potd done

--- leetcodedaily.java
+++ leetcodedaily.java
@@ -1244,3 +1244,33 @@
 //         return yd == 0 && xD == 0;
 //     }
 // }
+class Solution {
+    public int robotSim(int[] commands, int[][] obstacles) {
+        HashSet<String> hset = new HashSet<>();
+        for(int obs[] : obstacles){
+            hset.add(obs[0] + "#" + obs[1]);
+        }
+        int x =0,y=0;
+        int dir[][] = {{0,1},{1,0},{0,-1},{-1,0}};
+        int pos=0;
+        int ans=0;
+        for(int comm : commands){
+            if(comm == -1){
+                pos=(pos+1)%4;
+            }else if(comm == -2){
+                pos=(pos +3)%4;
+            }else{
+                for(int i=0;i<comm;i++){
+                    String key = (x + dir[pos][0] ) + "#" + (y + dir[pos][1]);
+                    if(hset.contains(key)) break;
+                    else {
+                        x = x + dir[pos][0];
+                        y =  y + dir[pos][1];
+                    }
+                }
+                ans = Math.max(ans,x*x+y*y);
+            }
+        }
+        return ans;
+    }
+}
