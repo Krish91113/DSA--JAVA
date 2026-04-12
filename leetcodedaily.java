066077cfca6c4fcb91563871f89e5dc3977a0261
Krish Baranwal <menikabaranwal121@gmail.com>
2026-04-12 09:30:53 +0530

    one potd done

--- leetcodedaily.java
+++ leetcodedaily.java
@@ -1430,3 +1430,15 @@ class Solution {
         return Math.min(costDown, costUp);
     }
 }
+class Solution {
+    public int[] findDegrees(int[][] matrix) {
+        int n = matrix.length;
+        int ans[] = new int[n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                ans[i]+=matrix[i][j]; 
+            }
+        }
+        return ans;
+    }
+}©leetcode
