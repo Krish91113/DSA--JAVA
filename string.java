264155c80ce7a743a111182dea5ba2300327d9da
Krish Baranwal <menikabaranwal121@gmail.com>
2026-04-21 20:02:06 +0530

    todays potd done

--- string.java
+++ string.java
@@ -627,3 +627,41 @@ class Solution {
         return ans;
     }
 }
+class Solution {
+    public int maxDistance(int[] colors) {
+        int ans = Integer.MIN_VALUE;
+        for(int i=0;i<colors.length;i++){
+            for(int j=i+1;j<colors.length;j++){
+                if(colors[i]!=colors[j]){
+                    ans = Math.max(ans,Math.abs(j-i));
+                }
+            }
+        }
+        return ans;
+    }
+}
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int result = nums[0] + nums[1] + nums[2]; // Initial best guess
+
+        for (int i = 0; i < n - 2; i++) {
+            int left = i + 1, right = n - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (Math.abs(target - sum) < Math.abs(target - result)) {
+                    result = sum;
+                }
+
+                if (sum == target) return target;
+                else if (sum < target) left++;
+                else right--;
+            }
+        }
+
+        return result;
+    }
+}
