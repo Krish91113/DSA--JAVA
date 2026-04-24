cb68cda4a699bad5599ec03ce870d4b79014df44
Krish Baranwal <menikabaranwal121@gmail.com>
2026-04-24 22:29:43 +0530

    todays potd done

--- string.java
+++ string.java
@@ -742,3 +742,14 @@ public boolean hasTripletSum(int arr[], int target) {
         return ans;
     }
 }
+class Solution {
+    public int furthestDistanceFromOrigin(String moves) {
+        int l=0,r=0,b=0;
+        for(char c : moves.toCharArray()){
+            if(c == 'L') l++;
+            else if(c == 'R') r++;
+            else b++;
+        }
+        return Math.abs(l-r) + b;
+    }
+}
