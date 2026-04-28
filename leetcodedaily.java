c8fdcffaa11da942715fc8d32f722235b18e778d
Krish Baranwal <menikabaranwal121@gmail.com>
2026-04-28 12:51:46 +0530

    first commit

--- leetcodedaily.java
+++ leetcodedaily.java
@@ -1541,3 +1541,25 @@ class Solution {
         return true;
     }
 }
+
+class Solution {
+    public int minOperations(int[][] grid, int x) {
+        int m = grid.length,n=grid[0].length;
+        int arr[] = new int[m*n];
+        int rem = grid[0][0]%x;
+        int id=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]%x != rem) return -1;
+                arr[id++] = grid[i][j];
+            }
+        }
+        Arrays.sort(arr);
+        int mid = arr.length/2;
+        int ans = 0;
+        for(int i=0;i<arr.length;i++){
+            ans+= (Math.abs(arr[mid] - arr[i])/x);
+        }
+        return ans;
+    }
+}
