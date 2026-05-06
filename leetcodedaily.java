a92204ad94a47fb6fefebad8dfe54a696419a67e
Krish Baranwal <menikabaranwal121@gmail.com>
2026-05-06 23:26:00 +0530

    first commit

--- leetcodedaily.java
+++ leetcodedaily.java
@@ -1725,3 +1725,27 @@
 //         return a;
 //     }
 // }
+class Solution {
+    public char[][] rotateTheBox(char[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+        char[][] res = new char[cols][rows];
+        
+        for (int r = 0; r < cols; r++)
+            for (int c = 0; c < rows; c++)
+                res[r][c] = '.';
+
+        for (int r = 0; r < rows; r++) {
+            int p = cols - 1;
+            for (int c = cols - 1; c >= 0; c--) {
+                if (grid[r][c] == '*') {
+                    res[c][rows - 1 - r] = '*';
+                    p = c - 1;
+                } else if (grid[r][c] == '#')
+                    res[p--][rows - 1 - r] = '#';
+            }
+        }
+
+        return res;
+    }
+}
