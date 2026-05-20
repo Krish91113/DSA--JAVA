e14247cae732e7e26b1bc8170eddaaf1676e3ccd
Krish Baranwal <menikabaranwal121@gmail.com>
2026-05-20 23:32:49 +0530

    todays otd done

--- leetcodedaily.java
+++ leetcodedaily.java
@@ -2023,3 +2023,25 @@
 //         return min == Integer.MAX_VALUE?-1:min;
 //     }
 // }
+class Solution {
+    public int[] findThePrefixCommonArray(int[] A, int[] B) {
+        int n = A.length;
+        int[] ans = new int[n];
+        // We'll use sets to track seen elements
+        Set<Integer> setA = new HashSet<>();
+        Set<Integer> setB = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            setA.add(A[i]);
+            setB.add(B[i]);
+            // Count common elements so far
+            int common = 0;
+            for (int num : setA) {
+                if (setB.contains(num)) {
+                    common++;
+                }
+            }
+            ans[i] = common;
+        }
+        return ans;
+    }
+}
