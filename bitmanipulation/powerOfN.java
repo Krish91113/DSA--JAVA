ad9d60ec974d1a32cd9e444113ba7cd7befd70a7
Krish Baranwal <menikabaranwal121@gmail.com>
2026-05-23 18:23:04 +0530

    potd done

---
+++ bitmanipulation/powerOfN.java
@@ -0,0 +1,8 @@
+package bitmanipulation;
+public class powerOfN {
+    public static void main(String[] args) {
+        int n = 16;
+        int m = 1;
+        System.out.println(m<<n);
+    }   
+}
--- leetcodedaily.java
+++ leetcodedaily.java
@@ -2078,3 +2078,15 @@ class Solution {
         return maxLen;
     }
 }
+
+class Solution {
+    public boolean check(int[] nums) {
+        int c = 0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] > nums[(i+1) % nums.length]){
+                c++;
+            }
+        }
+        return c<=1;
+    }
+}
