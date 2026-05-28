87ed686f8a40d2e79410e12408f8ca60cf578f61
Krish Baranwal <menikabaranwal121@gmail.com>
2026-05-28 23:46:30 +0530

    potd done

--- leetcodedaily.java
+++ leetcodedaily.java
@@ -2204,3 +2204,72 @@
 //         return count;
 //     }
 // }
+class Solution {
+    public int[] stringIndices(String[] words, String[] query) {
+        Trie trie = new Trie();
+        int index = -1, smallest = Integer.MAX_VALUE;
+        // Insert words into the Trie and track the shortest word's index
+        for (int i = 0; i < words.length; i++) {
+            trie.insert(words[i], i); // <-- Pass both word and index
+            if (words[i].length() < smallest) {
+                smallest = words[i].length();
+                index = i;
+            }
+        }
+        int[] ans = new int[query.length];
+        for (int i = 0; i < query.length; i++) {
+            int ind = trie.prefix(query[i]);
+            ans[i] = (ind == -1) ? index : ind;
+        }
+        return ans;
+    }
+}
+
+class TrieNode {
+    int index;
+    int len;
+    TrieNode[] children;
+
+    TrieNode() {
+        index = -1;
+        len = Integer.MAX_VALUE;
+        children = new TrieNode[26];
+    }
+}
+
+class Trie {
+    TrieNode root;
+
+    Trie() {
+        root = new TrieNode();
+    }
+
+    void insert(String str, int ind) {
+        TrieNode node = root;
+        int strlen = str.length();
+        // Insert characters in reverse for suffix matching
+        for (int i = strlen - 1; i >= 0; i--) {
+            int ch = str.charAt(i) - 'a';
+            if (node.children[ch] == null)
+                node.children[ch] = new TrieNode();
+            node = node.children[ch];
+            // Always keep the shortest and earliest index at each node
+            if (strlen < node.len || (strlen == node.len && ind < node.index)) {
+                node.len = strlen;
+                node.index = ind;
+            }
+        }
+    }
+
+    int prefix(String query) {
+        TrieNode node = root;
+        int ans = -1;
+        for (int i = query.length() - 1; i >= 0; i--) {
+            int ch = query.charAt(i) - 'a';
+            if (node.children[ch] == null) break;
+            node = node.children[ch];
+            ans = node.index;
+        }
+        return ans;
+    }
+}
