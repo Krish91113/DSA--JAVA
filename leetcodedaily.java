6cbef2fb50b1c94d627c7f9be0971a06d01e6822
Krish Baranwal <menikabaranwal121@gmail.com>
2026-06-10 23:35:05 +0530

    single no done

--- leetcodedaily.java
+++ leetcodedaily.java
@@ -2378,3 +2378,14 @@
 //         return maxLen;
 //     }
 // }
+class Solution {
+    public int hammingWeight(int n) {
+        int c =0;
+        while(n!= 0){
+            n = n & (n-1);
+            c++;
+        }
+        
+                return c;
+    }
+}
