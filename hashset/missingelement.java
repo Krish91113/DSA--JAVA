d3f39a38b9a2afc42b4aff29f8bb86c651c87bbe
Krish Baranwal <menikabaranwal121@gmail.com>
2026-06-23 16:22:53 +0530

    Add happy number solution with nextNumber method

--- hashset/missingelement.java
+++ hashset/missingelement.java
@@ -16,3 +16,24 @@ class Solution {
         return li;
     }
 }
+//happy number
+class Solution {
+    public static int nextNumber(int n){
+        int ans=0;
+        while(n > 0){
+            int rem = n % 10;
+            ans+=(rem*rem);
+            n/=10;
+        }
+        return ans;
+    }
+    public boolean isHappy(int n) {
+        Set<Integer> set=new HashSet<>();
+        while(!set.contains(n)){
+            set.add(n);
+            n=nextNumber(n);
+            if(n == 1) return true;
+        }
+        return false;
+    }
+}
