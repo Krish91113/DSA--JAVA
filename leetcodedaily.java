41bc5c9387272181d92934b88c4822474e318296
Krish Baranwal <menikabaranwal121@gmail.com>
2026-07-06 13:19:12 +0530

    change done

--- leetcodedaily.java
+++ leetcodedaily.java
@@ -2551,3 +2551,22 @@
 //     public int numberOfSubarrays(int[] nums, int k) {
 //         return countAtMost(nums, k) - countAtMost(nums, k - 1);
 //     }
+class Solution {
+    public int removeCoveredIntervals(int[][] intervals) {
+        Arrays.sort(intervals, (a,b) -> {
+            if(a[0] == b[0]){
+                return b[1]-a[1];
+            }
+            return a[0]-b[0];
+        });
+        int c =0;
+        int max=-1;
+        for(int[] interval : intervals){
+            if(interval[1] > max){
+                c++;
+                max=interval[1];
+            }
+        }
+        return c;
+    }
+}
