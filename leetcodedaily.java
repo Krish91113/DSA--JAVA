9470157ac75df3cb2d383a665679c8516992362b
Krish Baranwal <menikabaranwal121@gmail.com>
2026-07-07 22:50:32 +0530

    change done

--- leetcodedaily.java
+++ leetcodedaily.java
@@ -2570,3 +2570,35 @@
 //         return c;
 //     }
 // }
+class Solution {
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+
+        int[] candies = new int[n];
+
+        // Step 1: Everyone gets at least one candy
+        Arrays.fill(candies, 1);
+
+        // Step 2: Left -> Right
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+
+        // Step 3: Right -> Left
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+            }
+        }
+
+        // Step 4: Calculate answer
+        int ans = 0;
+        for (int candy : candies) {
+            ans += candy;
+        }
+
+        return ans;
+    }
+}
