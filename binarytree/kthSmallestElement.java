64a62ccfd6aea249c20dbdaaa83a6a295a1a9af4
Krish Baranwal <menikabaranwal121@gmail.com>
2026-07-28 08:33:53 +0530

    potd done

--- binarytree/kthSmallestElement.java
+++ binarytree/kthSmallestElement.java
@@ -1,132 +1,132 @@
-// import javax.swing.tree.TreeNode;
+// // import javax.swing.tree.TreeNode;
 
-// public class kthSmallestElement {
-//     static int ans;
-//     static int k2;
+// // public class kthSmallestElement {
+// //     static int ans;
+// //     static int k2;
 
-//     public static void main(String[] args) {
+// //     public static void main(String[] args) {
 
     
-//     private static void inorder(TreeNode root){
-//         if(root == null) return;
-//         inorder(root.left);
-//         k2--;
-//         if(k2 == 0) ans= root.val;
-//         inorder(root.right);
-//     }
-//     public int kthSmallest(TreeNode root, int k) {
-//         k2=k;
-//         ans= -1;
-//         inorder(root);
-//         return ans;
-//     }
+// //     private static void inorder(TreeNode root){
+// //         if(root == null) return;
+// //         inorder(root.left);
+// //         k2--;
+// //         if(k2 == 0) ans= root.val;
+// //         inorder(root.right);
+// //     }
+// //     public int kthSmallest(TreeNode root, int k) {
+// //         k2=k;
+// //         ans= -1;
+// //         inorder(root);
+// //         return ans;
+// //     }
 
-//     }
-// }
+// //     }
+// // }
 
-// class Solution {
-//     public List<List<Integer>> shiftGrid(int[][] grid, int k) {
+// // class Solution {
+// //     public List<List<Integer>> shiftGrid(int[][] grid, int k) {
 
-//         int m = grid.length;
-//         int n = grid[0].length;
+// //         int m = grid.length;
+// //         int n = grid[0].length;
 
-//         int total = m * n;
+// //         int total = m * n;
 
-//         k %= total;
+// //         k %= total;
 
-//         List<List<Integer>> ans = new ArrayList<>();
+// //         List<List<Integer>> ans = new ArrayList<>();
 
-//         for (int i = 0; i < m; i++) {
-//             List<Integer> row = new ArrayList<>();
+// //         for (int i = 0; i < m; i++) {
+// //             List<Integer> row = new ArrayList<>();
 
-//             for (int j = 0; j < n; j++) {
-//                 row.add(0);
-//             }
+// //             for (int j = 0; j < n; j++) {
+// //                 row.add(0);
+// //             }
 
-//             ans.add(row);
-//         }
+// //             ans.add(row);
+// //         }
 
-//         for (int i = 0; i < m; i++) {
-//             for (int j = 0; j < n; j++) {
+// //         for (int i = 0; i < m; i++) {
+// //             for (int j = 0; j < n; j++) {
 
-//                 int oldIndex = i * n + j;
+// //                 int oldIndex = i * n + j;
 
-//                 int newIndex = (oldIndex + k) % total;
+// //                 int newIndex = (oldIndex + k) % total;
 
-//                 int newRow = newIndex / n;
-//                 int newCol = newIndex % n;
+// //                 int newRow = newIndex / n;
+// //                 int newCol = newIndex % n;
 
-//                 ans.get(newRow).set(newCol, grid[i][j]);
-//             }
-//         }
+// //                 ans.get(newRow).set(newCol, grid[i][j]);
+// //             }
+// //         }
 
-//         return ans;
-//     }
-// }
+// //         return ans;
+// //     }
+// // }
 
-// import java.util.Arrays;
+// // import java.util.Arrays;
 
-// class Solution {
-//     // 1. Keep it non-static so it resets every test case
-//     private long[][] dp;
+// // class Solution {
+// //     // 1. Keep it non-static so it resets every test case
+// //     private long[][] dp;
 
-//     public long solve(int i, int[] nums, int flag) {
-//         if (i >= nums.length) return 0;
-//         if (dp[i][flag] != -1) return dp[i][flag];
-//         long skip = solve(i + 1, nums, flag);
-//         long val = nums[i];
-//         if (flag == 0) { 
-//             val = -val;
-//         }
-//         long take = solve(i + 1, nums, 1 - flag) + val;
-//         return dp[i][flag] = Math.max(take, skip);
-//     }
+// //     public long solve(int i, int[] nums, int flag) {
+// //         if (i >= nums.length) return 0;
+// //         if (dp[i][flag] != -1) return dp[i][flag];
+// //         long skip = solve(i + 1, nums, flag);
+// //         long val = nums[i];
+// //         if (flag == 0) { 
+// //             val = -val;
+// //         }
+// //         long take = solve(i + 1, nums, 1 - flag) + val;
+// //         return dp[i][flag] = Math.max(take, skip);
+// //     }
 
-//     public long maxAlternatingSum(int[] nums) {
-//         int n = nums.length;
-//         dp = new long[n][2];         
-//         for (long[] row : dp) {
-//             Arrays.fill(row, -1);
-//         }        
-//         return solve(0, nums, 1); 
-//     }
-// }
+// //     public long maxAlternatingSum(int[] nums) {
+// //         int n = nums.length;
+// //         dp = new long[n][2];         
+// //         for (long[] row : dp) {
+// //             Arrays.fill(row, -1);
+// //         }        
+// //         return solve(0, nums, 1); 
+// //     }
+// // }
+// // class Solution {
+// //     public int uniqueXorTriplets(int[] nums) {
+// //         int n=nums.length;
+// //         if(n==1 || n==2) return n;
+// //         int ans =1;
+// //         while(ans<=n){
+// //             ans*=2;
+// //         }
+// //         return ans;
+// //     }
+// // }
 // class Solution {
 //     public int uniqueXorTriplets(int[] nums) {
 //         int n=nums.length;
-//         if(n==1 || n==2) return n;
-//         int ans =1;
-//         while(ans<=n){
-//             ans*=2;
+//         HashSet<Integer> s1 = new HashSet<>();
+//         for(int i=0;i<n;i++){
+//             for(int j=i;j<n;j++){
+//                 s1.add(nums[i] ^ nums[j]);
+//             }
 //         }
-//         return ans;
+//         HashSet<Integer> s2 = new HashSet<>();
+//         for(int number : s1){
+//             for(int j=0;j<n;j++){
+//                 s2.add( number ^ nums[j]);
+//             }
+//         } 
+//         return s2.size();
 //     }
 // }
-class Solution {
-    public int uniqueXorTriplets(int[] nums) {
-        int n=nums.length;
-        HashSet<Integer> s1 = new HashSet<>();
-        for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                s1.add(nums[i] ^ nums[j]);
-            }
-        }
-        HashSet<Integer> s2 = new HashSet<>();
-        for(int number : s1){
-            for(int j=0;j<n;j++){
-                s2.add( number ^ nums[j]);
-            }
-        } 
-        return s2.size();
-    }
-}
-class Solution {
-    public int maxProduct(int n) {
-        char arr[] = Integer.toString(n).toCharArray();
-        Arrays.sort(arr);
-        int num1 = arr[arr.length - 1] - '0';
-        int num2 = arr[arr.length - 2] - '0';
+// class Solution {
+//     public int maxProduct(int n) {
+//         char arr[] = Integer.toString(n).toCharArray();
+//         Arrays.sort(arr);
+//         int num1 = arr[arr.length - 1] - '0';
+//         int num2 = arr[arr.length - 2] - '0';
         
-        return num1 * num2;
-    }
-}
+//         return num1 * num2;
+//     }
+// }
