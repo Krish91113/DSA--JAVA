23626da40c4eae951a6e3ebd92a232359124e510
Krish Baranwal <menikabaranwal121@gmail.com>
2026-09-06 22:10:20 +0530

    Update leetcodedaily.java today's pots done

--- leetcodedaily.java
+++ leetcodedaily.java
@@ -44,7 +44,37 @@ class Solution {
             }
         }
         return -1;
-}// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // r21// // // // public class leetcodedaily {
+
+}
+import java.util.Arrays;
+
+class Solution {
+    public static int solve(String s, String t, int m, int n, int[][] dp) {
+        if (n == 0) return 1;
+        if (m == 0) return 0;
+        if (dp[m][n] != -1) return dp[m][n];
+        
+        if (s.charAt(m - 1) == t.charAt(n - 1)) {
+            return dp[m][n] = solve(s, t, m - 1, n - 1, dp) + solve(s, t, m - 1, n, dp);
+        } else {
+            return dp[m][n] = solve(s, t, m - 1, n, dp);
+        }
+    }
+
+    public int numDistinct(String s, String t) {
+        int m = s.length();
+        int n = t.length();
+        
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i <= m; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+        
+        return solve(s, t, m, n, dp);
+    }
+}
+
+/ // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // r21// // // // public class leetcodedaily {
 // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //     public static int findClosest(int x,int y, int z){
 // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //         int distance1=Math.abs(x-z);
 // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //         int distance2=Math.abs(y-z);
