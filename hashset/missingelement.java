d378a6c10b381391e2d4385054393bd958740c16
Krish Baranwal <menikabaranwal121@gmail.com>
2026-09-10 07:33:04 +0530

    done with todays potd

--- hashset/missingelement.java
+++ hashset/missingelement.java
@@ -422,3 +422,43 @@
 //         return totalCommas;
 //     }
 // }
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Pair{
+    int sum;
+    int count;
+    Pair(int sum, int c){
+        this.sum=sum;
+        this.count=c;
+    }
+}
+class Solution {
+    int ans=0;
+    public int averageOfSubtree(TreeNode root) {
+        dfs(root);
+        return ans;
+    }
+    Pair dfs(TreeNode root){
+        if(root == null) return new Pair(0,0);
+        Pair left=dfs(root.left);
+        Pair right=dfs(root.right);
+        int sum = left.sum + right.sum + root.val;
+        int count = left.count + right.count + 1;
+        if(root.val == (sum/count)) ans++;
+        return new Pair(sum, count);
+    }
+}
