23e0756d4a1e4e243f57c2701d969569478fa453
Krish Baranwal <menikabaranwal121@gmail.com>
2026-09-18 16:22:10 +0530

    done with todays potd

--- hashset/missingelement.java
+++ hashset/missingelement.java
@@ -611,3 +611,27 @@
 //         return (int) dp[n - 1][k];
 //     }
 // }
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean checkTree(TreeNode root) {
+        int a = root.val;
+        int b=root.left.val;
+        int c=root.right.val;
+        if(a!=(b+c)) return false;
+        return true;
+    }
+}
